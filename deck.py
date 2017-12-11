015f0ad3ebce447af12a61152b5fc7ab1cf0fdbf
leizou <346954742@qq.com>
2017-12-11 13:40:29 +0800

    sync

--- deck.py
+++ deck.py
@@ -4,7 +4,7 @@ import itertools
 import handsrange
 
 def generateCards(handsstr):
-    trandict = {"T": 10, "t": 10, "J": 11, "Q": 12, "K": 13, "A": 14, "j": 11, "q": 12, "k": 13, "a": 14, 1:14,
+    trandict = {"T": 10, "t": 10, "J": 11, "Q": 12, "K": 13, "A": 14, "j": 11, "q": 12, "k": 13, "a": 14, "1":14,
                 "S": 0,"s": 0,"H": 1, "h": 1, "C": 2, "c": 2, "D": 3, "d": 3}
     cardsList = []
     handsstr = handsstr.replace(" ","")
@@ -21,7 +21,7 @@ def generateCards(handsstr):
     return cardsList
 
 def generateHands(handsstr):
-    trandict = {"T":10,"t":10,"J":11,"Q":12,"K":13,"A":14,"j":11,"q":12,"k":13,"a":14,1:14,
+    trandict = {"T":10,"t":10,"J":11,"Q":12,"K":13,"A":14,"j":11,"q":12,"k":13,"a":14,"1":14,
                 "S":0,"s":0,"H":1,"h":1,"C":2,"c":2,"D":3,"d":3}
     if len(handsstr) == 5:
         handsstr = handsstr[:2] + handsstr[3:]
